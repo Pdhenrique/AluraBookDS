d33cebc0795b948f0526143703b799d2808ec16f
Pedro Santana <pedro.ana@nexti.com>
2023-08-29 15:01:22 -0300

    docs: added button template variants

--- DS-AluraBook/stories/AbButton.stories.tsx
+++ DS-AluraBook/stories/AbButton.stories.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 
-import { AbButton } from '../src';
+import { AbButton, abButtonProps } from '../src';
 
 export default {
+  argTypes: {
+    children: {
+      control: { type: 'text' },
+      defaultValue: 'Button',
+      name: 'children',
+    },
+    variant: {
+      control: { type: 'select' },
+      defaultValue: 'primary',
+      name: 'variant',
+      options: ['primary', 'secondary'],
+    },
+  },
+
   title: 'Component/AbButton',
 } as ComponentMeta<typeof AbButton>;
 
-const Template: ComponentStory<typeof AbButton> = () => <AbButton />;
+const Template: ComponentStory<typeof AbButton> = args => (
+  <AbButton {...args} />
+);
 
-export const Component = Template.bind({});
+export const ComponentPrimary = Template.bind({});
+
+ComponentPrimary.args = { variant: 'primary' } as abButtonProps;
+
+export const ComponentSecondary = Template.bind({});
+
+ComponentSecondary.args = { variant: 'secondary' } as abButtonProps;
