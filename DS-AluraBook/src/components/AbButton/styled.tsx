64f5be4ff3db6f0589741bab50bda549b5741115
Pedro Santana <pedro.ana@nexti.com>
2023-08-29 15:00:19 -0300

    style: update button stylization

--- DS-AluraBook/src/components/AbButton/styled.tsx
+++ DS-AluraBook/src/components/AbButton/styled.tsx
@@ -1,17 +1,33 @@
-import { styled } from 'styled-components';
+import { css, styled } from 'styled-components';
+import { abButtonProps } from '.';
 
-export const AbBtnPrimary = styled.button`
-  background-color: #eb9b00;
+export const AbBtnPrimary = styled.button<abButtonProps>`
+  background-color: ${(props: abButtonProps) =>
+    props.variant === 'primary' ? '#eb9b00' : '#fff'};
+
   border: 2px solid #eb9b00;
-  color: #ffffff;
+
+  color: ${(props: abButtonProps) =>
+    props.variant === 'primary' ? '#fff' : '#eb9b00'};
 
   font-size: 20px;
   padding: 16px 32px 16px 32px;
 
   cursor: pointer;
 
-  &:hover {
-    background-color: #b87900;
-    border-color: #b87900;
-  }
+  ${(props: abButtonProps) =>
+    props.variant === 'primary'
+      ? css`
+          &:hover {
+            background-color: #b87900;
+            border-color: #b87900;
+          }
+        `
+      : css`
+          &:hover {
+            background-color: #fff;
+            border-color: #b87900;
+            color: #b87900;
+          }
+        `};
 `;
